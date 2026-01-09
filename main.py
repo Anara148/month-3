815a4525ea8bb6d1e0649eecbe32f1019e14ed12
Anara148 <anaraberenbaeva93@gmail.com>
2026-01-10 00:39:37 +0600

    hw_3

--- main.py
+++ main.py
@@ -1,76 +1,43 @@
 import flet as ft
-from datetime import datetime
+from datetime import datetime 
 
-
-def main(page: ft.Page):
+def main (page: ft.Page):
     page.title = "Мое первое приложение!"
-    page.theme_mode = ft.ThemeMode.DARK
+    page. theme_mode = ft.ThemeMode. DARK
 
-    greeting_history = []
-    favorite_names = []
-
-    greeting_text = ft.Text('История приветствий:')
-
-    favorite_text = ft.Text('Избранные имена:')
 
     text_hello = ft.Text(value='Hello world')
 
-
     def on_button_click(_):
-        name = name_input.value.strip()
-        print(name)
+        name = (name_input.value or "").strip()
+        print (name)
 
         if name:
-                hour = datetime.now().hour
-                time_text = "утреннее время" if hour < 12 else "вечернее время"
-
-
-                text_hello.color = None
-                text_hello.value = f"Привет {name}! ({time_text})"
+            if len (name) < 3:
+                text_hello.value = 'Имя слишком короткое'
+                text_hello. color = ft.Colors.RED
                 name_input.value = None
+            elif len(name) >= 20:
+                text_hello.value = 'Имя слишком длинное'
+                text_hello.color = ft.Colors. RED
+                name_input.value = None
+            else:
+                now = datetime.now()
+                formatted_time = now.strftime("%Y-%m-%d / %H:%M:%S")
 
-                greeting_history.append(name)
-                #print(greeting_history)
-                greeting_text.value = 'История приветствий: \n' + "\n".join(greeting_history)
-
+                text_hello.color =  None
+                text_hello.value = f"{formatted_time} - Привет {name}!"
+                name_input.value = None
         else:
             text_hello.value = 'Введите корректное имя'
             text_hello.color = ft.Colors.RED
 
-    def add_to_favorite(_):
-        if not greeting_history:
-            text_hello.value = 'Сначала добавьте имя'
-            text_hello.color = ft.Colors.RED
-        else: 
-            last_name = greeting_history[-1]
-            if last_name not in favorite_names:
-                favorite_names.append(last_name)
-                favorite_text.value = 'Избранные имена: \n' + "\n".join(favorite_names)
-            text_hello.value = f"{last_name}  добавлено в избранное"
-            text_hello.color = None
 
+    elevated_button = ft.ElevatedButton('SEND', icon=ft.Icons. SEND, on_click=on_button_click)
 
-    elevated_button = ft.ElevatedButton('SEND', icon=ft.Icons.SEND, on_click=on_button_click)
+    name_input = ft.TextField(label='Введите имя', max_length=20, on_submit=on_button_click)
 
-    def clear_history(_):
-        greeting_history.clear()
-        greeting_text.value = 'История приветствий:' 
+    page.add(text_hello, name_input, elevated_button)
 
+ft. app(target=main)
 
-    clear_button = ft.IconButton(icon=ft.Icons.DELETE, on_click=clear_history)
-
-    favorite_button = ft.ElevatedButton("⭐ В избранное", on_click=add_to_favorite)
-
-    name_input = ft.TextField(label='Введите имя', on_submit=on_button_click, expand=True)
-
-    main_object = ft.Row([name_input, elevated_button, clear_button])
-
-    text_row = ft.Row([text_hello], alignment=ft.MainAxisAlignment.CENTER)
-
-    header_row = ft.Row([greeting_text, favorite_text], alignment=ft.MainAxisAlignment.START)
-
-    page.add(text_row, main_object, favorite_button, header_row)
-
-
-
-ft.app(target=main)
